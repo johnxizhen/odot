25827fab7741c636181a9669eec087b447e02830
johnxizhen <xizhenlijohn1986@gmail.com>
2015-01-19 05:12:06 +0000

    Clean up todo mark completion view

--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -2,4 +2,22 @@ require 'spec_helper'
 
 describe TodoItem do
   it { should belong_to(:todo_list) }
+  
+  describe "#completed?" do
+    let(:todo_item) { TodoItem.create(content: "Hello") }
+    
+    # be_false is deprecated, use be false or be_falsey instead.
+    
+    it "is false when completed_at is blank" do
+      todo_item.completed_at = nil
+      expect(todo_item.completed?).to be_falsey
+    end    
+    
+    # be_true is deprecated, use be true or be_truthy instead.    
+    
+    it "returns true when completed_at is not empty" do
+      todo_item.completed_at = Time.now
+      expect(todo_item.completed?).to be_truthy
+    end
+  end
 end
